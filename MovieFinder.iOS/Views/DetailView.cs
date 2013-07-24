246462f1b8b1f67e5dfd1ab230b63243efbe0220
Stefan Schoeb <opendix@gmail.com>
2013-07-24 07:55:24 +0200

    Remove unused stuff from iOS project / Setup views for iOS

---
+++ MovieFinder.iOS/Views/DetailView.cs
@@ -0,0 +1,39 @@
+﻿using System.Drawing;
+using Cirrious.MvvmCross.Binding.BindingContext;
+using Cirrious.MvvmCross.Touch.Views;
+using MonoTouch.UIKit;
+using MovieFinder.Core.ViewModels;
+
+namespace MovieFinder.iOS.Views
+{
+    public class DetailView : MvxViewController
+    {
+        public override void ViewDidLoad()
+        {
+            var textViewTitle = new UITextField(new RectangleF(10, 100, 300, 30));
+            Add(textViewTitle);
+
+            var textViewTitleValue = new UITextView(new RectangleF(40, 100, 300, 30));
+            Add(textViewTitleValue);
+
+            var textViewYear = new UITextField(new RectangleF(70, 100, 300, 30));
+            Add(textViewYear);
+
+            var textViewYearValue = new UITextView(new RectangleF(100, 100, 300, 30));
+            Add(textViewYearValue);
+
+            var textViewSynopsis = new UITextField(new RectangleF(130, 100, 300, 30));
+            Add(textViewSynopsis);
+
+            var textViewSynopsisValue = new UITextView(new RectangleF(160, 100, 300, 30));
+            Add(textViewSynopsisValue);
+
+
+            var bindingSet = this.CreateBindingSet<DetailView, DetailViewModel>();
+            bindingSet.Bind(textViewTitleValue).To(vm => vm.Movie.title);
+            bindingSet.Bind(textViewYearValue).To(vm => vm.Movie.year);
+            bindingSet.Bind(textViewSynopsisValue).To(vm => vm.Movie.synopsis);
+            bindingSet.Apply();
+        }
+    }
+}
--- MovieFinder.iOS/Views/FirstView.cs
+++ MovieFinder.iOS/Views/FirstView.cs
@@ -1,28 +1,45 @@
+using System;
+using System.Collections.Generic;
 using System.Drawing;
 using Cirrious.MvvmCross.Binding.BindingContext;
+using Cirrious.MvvmCross.Binding.Touch.Views;
 using Cirrious.MvvmCross.Touch.Views;
 using MonoTouch.UIKit;
 using MonoTouch.Foundation;
+using MovieFinder.Core.ViewModels;
 
 namespace MovieFinder.iOS.Views
 {
     [Register("FirstView")]
-    public class FirstView : MvxViewController
+    public class FirstView : MvxCollectionViewController
     {
+        protected FirstView() : base("FirstView", null)
+        {
+        }
+
         public override void ViewDidLoad()
         {
-            View = new UIView(){ BackgroundColor = UIColor.White};
-            base.ViewDidLoad();
+            var tableView = new UITableView(new RectangleF(60, 10, 300, 400));
+            Add(tableView);
 
-            var label = new UILabel(new RectangleF(10, 10, 300, 40));
-            Add(label);
-            var textField = new UITextField(new RectangleF(10, 50, 300, 40));
-            Add(textField);
+            var editView = new UITextView(new RectangleF(10, 10, 200, 30));
+            Add(editView);
 
-            //var set = this.CreateBindingSet<FirstView, Core.ViewModels.FirstViewModel>();
-            //set.Bind(label).To(vm => vm.);
-            //set.Bind(textField).To(vm => vm.Hello);
-            //set.Apply();
+            var searchButton = new UIButton(new RectangleF(220, 10, 80, 30));
+            searchButton.SetTitle("Search", UIControlState.Normal);
+            Add(searchButton);
+
+            var source = new MvxStandardTableViewSource(tableView, "TitleText Name;");
+
+            var bindingSet = this.CreateBindingSet<FirstView, FirstViewModel>();
+            bindingSet.Bind(tableView).To(viewModel => viewModel.Movies);
+            bindingSet.Bind(editView).To(viewModel => viewModel.Keyword);
+            bindingSet.Bind(searchButton).To(viewModel => viewModel.SearchCommand);
+            bindingSet.Bind(source).For(x => x.SelectionChangedCommand).To(viewModel => viewModel.ShowDetailCommand);
+            bindingSet.Apply();
+
+            tableView.Source = source;
+            tableView.ReloadData();
         }
     }
 }
