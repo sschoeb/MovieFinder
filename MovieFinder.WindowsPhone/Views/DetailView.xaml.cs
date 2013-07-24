ed42ed5ee6ef49b462544651eb8ce6206391eb84
Stefan Schoeb <opendix@gmail.com>
2013-07-24 08:08:56 +0200

    Add windows phoe ui

---
+++ MovieFinder.WindowsPhone/Views/DetailView.xaml.cs
@@ -0,0 +1,12 @@
+using Cirrious.MvvmCross.WindowsPhone.Views;
+
+namespace MovieFinder.WindowsPhone.Views
+{
+    public partial class DetailView : MvxPhonePage
+    {
+        public DetailView()
+        {
+            InitializeComponent();
+        }
+    }
+}
