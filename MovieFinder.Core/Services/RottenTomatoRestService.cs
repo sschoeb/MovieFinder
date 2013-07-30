9523023d7ed636508d6fbb2dff349f45fb32fdcc
Stefan Schoeb <opendix@gmail.com>
2013-07-30 15:35:40 +0200

    Remove obsolte code

--- MovieFinder.Core/Services/RottenTomatoRestService.cs
+++ MovieFinder.Core/Services/RottenTomatoRestService.cs
@@ -40,36 +40,5 @@ namespace MovieFinder.Core.Services
                 callback(_jsonConverter.DeserializeObject<T>(resultString));
             }
         }
-
-        //public async Task<RootObject> SearchMoviesAsync(string keyword)
-        //{
-        //    return await DoRequest<RootObject>(string.Format(SearchUrl, ApiKey, keyword));
-        //}
-
-        //public async Task<Movie> RequestMovieDetailsAsync(int movieId)
-        //{
-        //    return await DoRequest<Movie>(string.Format(DetailUrl, ApiKey, movieId));
-        //}
-
-        //private async Task<T> DoRequest<T>(string url)
-        //{
-        //    var client = new HttpClient();
-        //    HttpResponseMessage message = await client.GetAsync(url);
-
-        //    if (message.IsSuccessStatusCode)
-        //    {
-        //        using (var stream = await message.Content.ReadAsStreamAsync())
-        //        {
-        //            using (var streamReader = new StreamReader(stream))
-        //            {
-        //                string content = await streamReader.ReadToEndAsync();
-        //                T result = await JsonConvert.DeserializeObjectAsync<T>(content);
-        //                return result;
-        //            }
-        //        }
-        //    }
-
-        //    return default(T);
-        //}
     }
 }
